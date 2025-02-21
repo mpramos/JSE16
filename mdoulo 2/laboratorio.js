d6a5d1c6e8b914ffa22a52da2d7d87ef75cce1d4
maria <mpramos78915400@gmail.com>
2025-02-20 21:59:04 -0400

    fin del modulo 1

---
+++ mdoulo 2/laboratorio.js
@@ -0,0 +1,11 @@
+// Usando los mismos datos que en el laboratorio anterior, cree la lista de contactos como una matriz, 
+// cada elemento de la cual será un objeto que describe a un solo usuario. Por lo tanto, debemos 
+// obtener una matriz de tres elementos, y cada objeto colocado en ella contendrá tres piezas de 
+// información (nombre, teléfono y correo electrónico).
+
+// Al final de la lista declarada de esta manera, agregue un nuevo contacto utilizando el método de matriz
+//  apropiado. El nuevo contacto es: Maisie Haley / 0913 531 3030 / risus.Quisque@urna.ca.
+
+// Muestre el primer y el último contacto, nuevamente en el formato:
+//  nombre / teléfono / correo electrónico. Utilice la propiedad length de la matriz para determinar el 
+// índice del último elemento. Recuerde que los elementos de la matriz se indexan a partir de 0.
