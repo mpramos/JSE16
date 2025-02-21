c5aa6d6a9cd556ae59030be2f7be6b3522f639a3
maria <mpramos78915400@gmail.com>
2025-02-20 21:51:19 -0400

    fin del modulo 1

---
+++ proyectoIntegrador/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiscb4pIUQrmgssBG2iYiQ7X04Q9EyVDg",
+  authDomain: "inventoryjse16.firebaseapp.com",
+  projectId: "inventoryjse16",
+  storageBucket: "inventoryjse16.firebasestorage.app",
+  messagingSenderId: "645020112635",
+  appId: "1:645020112635:web:f9d37141df421fb456c25e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
